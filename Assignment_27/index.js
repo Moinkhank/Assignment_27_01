ba79465575a063503f97dcb1662ccf2ec5772d49
Moinkhank <92512673+Moinkhank@users.noreply.github.com>
2023-04-14 15:58:11 +0530

    Add files via upload

---
+++ Assignment_27/index.js
@@ -0,0 +1,12 @@
+
+const firstName = "Mohammad";
+const lastName = "Moin";
+const age = 22;
+const country = "INDIA";
+const city = "Bhopal";
+
+localStorage.setItem("firstName", firstName);
+localStorage.setItem("lastName", lastName);
+localStorage.setItem("age", age);
+localStorage.setItem("country", country);
+localStorage.setItem("city", city);
